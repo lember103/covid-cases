c8e58c0edb201868c9be6383206c1f03d461a911
Matthias Hiemer <matthias.hiemer@neuefische.de>
2021-09-22 16:08:06 +0200

    Initial empty Spring project

---
+++ src/main/java/de/neuefische/covidcases/CovidCasesApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.covidcases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidCasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidCasesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/covidcases/CovidCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.covidcases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidCasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
